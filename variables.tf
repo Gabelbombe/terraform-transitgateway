715748fec0d69159df27f4eac88046fef1cb18e6
ehime <dodomeki@gmail.com>
2019-03-28 10:37:32 -0700

    vars fixed

--- variables.tf
+++ variables.tf
@@ -1,23 +1,3 @@
-variable "access_key" {
-  default = ""
-}
-
-variable "secret_key" {
-  default = ""
-}
-
-variable "region" {
-  default = "ap-south-1"
-}
-
-variable "az1" {
-  default = "eu-central-1a"
-}
-
-variable "az2" {
-  default = "eu-central-1b"
-}
-
 variable "scenario" {
   default = "test-tgw"
 }
