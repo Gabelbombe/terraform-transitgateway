2c53770eac65548de77d716107914c4d1f6f8cf5
ehime <dodomeki@gmail.com>
2019-03-28 10:58:05 -0700

    idk

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ The Transit Gateway is meant to superseed the more complex and expensive Transit
 
 * ```hcl
 * module "gateway_mesh" {
-*   source      = "git@github.com:ehime/terraform-transitgateway?ref=v1.0.1"
+*   source      = "git@github.com:ehime/terraform-transitgateway?ref=v1.0.2"
 *   public_key  = "ssh-rsa AAAAB3NzaC1y...qd4hssndQ== rsa-key-20180518"
 *   scenario    = "tgw-mesh-scenario"
 * }
@@ -15,8 +15,8 @@ The Transit Gateway is meant to superseed the more complex and expensive Transit
 
 See [example](example) for a complete example ....
 
-## Architecture
 
+## Architecture
 A Transit Gateway relies on Route Tables. By default, a new Route Table is created in the Transit Gateway, which populates with the routing info toward every VPC attached to the gateway (the full mesh scenario)
 The Terraform code in this project demonstrates a more complex scenario in which traffic is isolated based on the environment. Four VPCs are created, with two subnets each (in separate AZs):
   - VPC-1: in the 'dev' environment
@@ -31,18 +31,18 @@ Let's assume the 'shared' environment will host shared components, such as proxy
 
 To enable such a scenario, three Route Tables are created in the Transit Gateway, one per environment. Which means both dev VPCs attach to the same Route Table, whereas the shared and prod VPCs each attach to their respective Route Table. Each VPC gets a t2.micro Ubuntu instance to validate the network connectivity over ssh and ICMP (ping). The instance in the 'shared' is assigned a public IP so a VPN connection isn't needed. Adding the necessary Terraform resources to establish a VPN connection could be an extension of this project.
 
-![transit-gateway-architecture](./assets/transit-gateway.png?raw=true "Transit Gateway Architecture")
+![transit-gateway-architecture](assets/transit-gateway.png "Transit Gateway Architecture")
 
 The thick green links on the diagram represents the authorized traffic through the gateway.
 
-## Prerequisites
 
+## Prerequisites
   - An AWS account
   - AWS Vault configured
   - Terraform
 
-## Documentation generation
 
+## Documentation generation
 Documentation should be modified within `main.tf` and generated using [terraform-docs](https://github.com/segmentio/terraform-docs):
 
 ```bash
@@ -50,7 +50,6 @@ terraform-docs md ./ |sed '$d' >| README.md
 ```
 
 ## License
-
 GPL 3.0 Licensed. See [LICENSE](https://github.com/ehime/terraform-transitgateway/tree/master/LICENSE) for full details.
 */
 
